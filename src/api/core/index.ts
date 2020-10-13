bc02f2bf74b7d3af1dd324edd8ab7a57e0deb189
yuuko-amamiya <yuuko-amamiya@protonmail.com>
2020-10-13 22:38:06 +0300

    api & deploy script

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,4 +1,6 @@
-export const apiUrl = "http://localhost:3001";
+const isDevelopment = process.env.NODE_ENV == 'development';
+
+export const apiUrl = isDevelopment ? "https://undefined-overflow.github.io/IllusionFakeApi" : "http://localhost:3001";
 
 export const apiRequest = (input: string, init?: RequestInit) =>
   fetch(`${apiUrl}/api/${input}`, init);
