cddf9020ad784994a7be70ebffdcd78db73edd21
yuuko-amamiya <yuuko-amamiya@protonmail.com>
2020-10-22 01:16:35 +0300

    messages

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,6 +1,6 @@
 const isDevelopment = process.env.NODE_ENV == 'development';
 
-export const apiUrl = isDevelopment ? "http://localhost:3001" : "https://undefined-overflow.github.io/IllusionFakeApi";
+export const apiUrl = isDevelopment ? "http://localhost:3001" : "https://undefined-overflow.github.io/IllusionContentManager";
 
 export const apiRequest = (input: string, init?: RequestInit) =>
   fetch(`${apiUrl}/api/${input}`, init);
